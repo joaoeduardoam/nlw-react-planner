201a0d8842b0677af29649063cff2a1cf670e704
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-07-09 21:13:29 -0300

    feat: Travel Registration Step 2 - Persons on trip

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,17 @@
-import {MapPin, Calendar, ArrowRight} from 'lucide-react'
+import {MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2} from 'lucide-react'
+import { useState } from 'react'
 
 
 export function App() {
+
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+
+  function openGuestsInput(){
+    setIsGuestsInputOpen(true)
+  }
+  function closeGuestsInput(){
+    setIsGuestsInputOpen(false)
+  }
 
   return (
     
@@ -14,24 +24,54 @@ export function App() {
         </div>
         
 
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+        <div className="space-y-4">
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+
+            <div className="flex items-center gap-2 flex-1">
+              <MapPin className="size-5 text-zinc-400"/>
+              <input disabled={isGuestsInputOpen} type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Calendar className="size-5 text-zinc-400"/>
+              <input disabled={isGuestsInputOpen} type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400 w-40  outline-none"/>
+            </div>
+
+            <div className="w-px h-6 bg-zinc-800"/>
+
+            {isGuestsInputOpen ? (
+              <button onClick={closeGuestsInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+              Change Local/Date
+              <Settings2 className='size-5'/>
+            </button>
+            ) : (
+              <button onClick={openGuestsInput} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              Continue
+              <ArrowRight className='size-5'/>
+            </button>
+            )}
+          </div>
+
+          {isGuestsInputOpen && (
+
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
 
           <div className="flex items-center gap-2 flex-1">
-            <MapPin className="size-5 text-zinc-400"/>
-            <input type="text" placeholder="Where are you going?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
-          </div>
-          
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400"/>
-            <input type="text" placeholder="When?" className="bg-transparent text-lg placeholder-zinc-400 w-40  outline-none"/>
+            <UserRoundPlus className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="Who will be on trip?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
           </div>
 
+
           <div className="w-px h-6 bg-zinc-800"/>
-          
+
           <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-            Continue
+            Confirm trip
             <ArrowRight className='size-5'/>
           </button>
+          </div>
+
+          )}
+
         </div>
 
         <p className="text-sm text-zinc-500">Ao planejar sua viagem pelo plann.er você automaticamente concorda <br />
